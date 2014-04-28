5e83b75fdf9731f9cf750cbf4bda261de9d90d92
Ayal Spitz <bitbucket@blackgecko.com>
2014-04-27 23:10:08 -0400

    Initial Commit

---
+++ ControllerTable/ZViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZViewController.h
+//  ControllerTable
+//
+//  Created by Ayal Spitz on 4/27/14.
+//  Copyright (c) 2014 Ayal Spitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZViewController : UIViewController
+
+@end
---
+++ ControllerTable/ZViewController.m
@@ -0,0 +1,29 @@
+//
+//  ZViewController.m
+//  ControllerTable
+//
+//  Created by Ayal Spitz on 4/27/14.
+//  Copyright (c) 2014 Ayal Spitz. All rights reserved.
+//
+
+#import "ZViewController.h"
+
+@interface ZViewController ()
+
+@end
+
+@implementation ZViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
